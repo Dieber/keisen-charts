cb033def12937d15ffb57cf226a98897cd7cfff0
Dieber <a451120068@gmail.com>
2026-07-30 15:42:37 +0800

    默认亮色模式

--- apps/website/docusaurus.config.ts
+++ apps/website/docusaurus.config.ts
@@ -110,7 +110,7 @@ const config: Config = {
     colorMode: {
       defaultMode: "light",
       disableSwitch: false,
-      respectPrefersColorScheme: true,
+      respectPrefersColorScheme: false,
     },
     navbar: {
       title: 'Keisen',
